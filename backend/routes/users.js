14c0e0d4dd69ae427d87907885a263a1a662eb45
unknown <monmaramba@yahoo.com>
2019-09-11 16:32:16 -0700

    comment editing route working

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,4 +21,27 @@ router.route("/add").post((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  User.findById(req.params.id).then(user => {
+    user.username = req.body.username;
+    user.email = req.body.email;
+    user.comment = req.body.comment;
+    user.date = Date.parse(req.body.date);
+
+    user
+      .save()
+      .then(() =>
+        res
+          .json("Comment updated")
+          .catch(err => res.status(4000).json("Error: " + err))
+      );
+  });
+});
+
 module.exports = router;
