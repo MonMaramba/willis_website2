6a66a767e4fe916e63706bd5069f7951539175bc
unknown <monmaramba@yahoo.com>
2019-09-11 19:26:03 -0700

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="container">Hello! Hello!</div>;
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component="{ExercisesList}" />
+      <Route path="/" exact component="{ExercisesList}" />
+      <Route path="/" exact component="{ExercisesList}" />
+      <Route path="/" exact component="{ExercisesList}" />
+    </Router>
+  );
 }
 
 export default App;
