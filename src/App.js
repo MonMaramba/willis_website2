3590165dfd9415004c70202bb9520b80edb342fb
unknown <monmaramba@yahoo.com>
2019-09-13 21:04:41 -0700

    logo complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/navbar.component";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <br />
-      <Route path="/" exact component="{Home}" />
-      <Route path="/" exact component="{Comments}" />
+      {/* <Route path="/" exact component="{Home}" /> */}
+      {/* <Route path="/" exact component="{Comments}" /> */}
     </Router>
   );
 }
---
+++ src/components/navbar.component.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <div>
+          <div className="left">
+            Luminocity
+            <span className="subtext">Light and Sound</span>
+          </div>
+        </div>
+        <div className="right">Blah Blah there</div>
+      </nav>
+    );
+  }
+}
