5d2f2801e1590d23f68fb1bafeaede81b2921ce4
unknown <monmaramba@yahoo.com>
2019-09-15 02:09:15 -0700

    navbar component css done

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,19 +1,38 @@
 import React, { Component } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar">
-        <div>
-          <div className="left">
-            Luminocity
-            <span className="subtext">Mobile Light and Sound</span>
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "rgb(53, 133, 199)",
+          boxShadow: "none",
+          padding: "10px 0px"
+        }}
+      >
+        <Toolbar>
+          <div className="header_container">
+            <div className="left">Luminocity</div>
+            <div className="subtext">Mobile Light and Sound</div>
           </div>
-        </div>
-        <div className="right">Blah Blah there</div>
-      </nav>
+          <div className="right">
+            <IconButton
+              aria-label="Menu"
+              color="inherit"
+              onClick={() => console.log("open")}
+            >
+              <MenuIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
