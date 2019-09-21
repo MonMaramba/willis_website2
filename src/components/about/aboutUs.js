edc3a46ae6012cf54b0312bcb93729f7f0ae653c
unknown <monmaramba@yahoo.com>
2019-09-20 21:14:20 -0700

    css tweak and code cleanup

--- src/components/about/aboutUs.js
+++ src/components/about/aboutUs.js
@@ -128,37 +128,7 @@ export default class AboutUs extends Component {
           {this.animateText()}
           {this.animateNext()}
           {this.animateLast()}
-          {/* <h2>About Us</h2>
-          <br />
-          <br />
-
-          <p>
-            Luminocity had humble beginnings in 2007 with a basic set of Mackie
-            speakers and a standard dj frontline. We mainly targeted events and
-            parties at small venues, homes and bars.
-          </p>
-          <p>
-            Ever since then, the equipment upgrades have been constant and we
-            now offer the latest technologies for lights and sounds.
-          </p>
-          <p>
-            Our regular clientele has also grown to include corporations,
-            non-profit organizations and families who always trust us to handle
-            their special events. We accommodate all kinds of requests for
-            entertainment.
-          </p>
-
-          <p>
-            We have also formed alliances and affiliations that help us support
-            one another so no requirement or venue will be too big or too
-            complex.
-          </p> */}
         </div>
-        {/* <div className="about_right">
-          <h6>Celebrating</h6>
-          {this.animateNumber()}
-          <h5>Years!</h5>
-        </div> */}
       </div>
     );
   }
