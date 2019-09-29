53690c4e2433117c746fab2605c2db010a51aa17
unknown <monmaramba@yahoo.com>
2019-09-28 17:52:20 -0700

    testimonial text change working

--- src/components/mainfeature/index.js
+++ src/components/mainfeature/index.js
@@ -4,26 +4,55 @@ import axios from "axios";
 
 export default class MainFeature extends Component {
   state = {
-    users: []
+    users: [],
+    textToShow: [],
+    currentText: ""
   };
+
+  newRandomNumber() {
+    return Math.floor(Math.random() * this.state.textToShow.length);
+  }
 
   componentDidMount() {
     axios
       .get("http://localhost:5000/users/")
       .then(response => {
         this.setState({ users: response.data });
-        console.log(this.state.users);
+        let text = this.state.users.map(
+          entry => `"${entry.comment}" - ${entry.username}`
+        );
+        this.setState({
+          textToShow: text
+        });
+        console.log(this.state.textToShow);
+
+        setInterval(() => {
+          let randomizer = Math.floor(
+            Math.random() * this.state.textToShow.length
+          );
+
+          this.setState({
+            currentText: this.state.textToShow[randomizer]
+          });
+        }, 10000);
       })
       .catch(error => {
         console.log(error);
       });
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutHandle);
+    this.setState({
+      textToShow: ""
+    });
   }
   render() {
     return (
       <div style={{ position: "relative" }}>
         <Carrousel />
         <div className="testimonial">
-          <div className="wrapper">Eat my shorts - Bart Simpson</div>
+          <div className="wrapper">{this.state.currentText}</div>
         </div>
       </div>
     );
